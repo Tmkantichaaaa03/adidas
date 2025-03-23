74dea9df169131f31e53f319e4adffd560d32792
Tmkantichaaaa03 <momonut6627@gmail.com>
2025-03-23 15:25:29 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:adidas/home_screen.dart';
+import 'package:adidas/search_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( HomeScreen());
+
+}
+
+
+
